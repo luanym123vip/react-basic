68966805fdd9c58e7033aa50e9a1fd8a699649b3
luanym123vip <camduyen20013@gmail.com>
2023-06-15 14:09:13 +0700

    handle Input

--- src/views/Examples/MyComponent.js
+++ src/views/Examples/MyComponent.js
@@ -2,17 +2,40 @@ import React from "react";
 class MyComponent extends React.Component {
 
     state = {
-        name: "Diên",
-        channel: "Diên cute",
+        firstName: '',
+        lastName: '',
     }
+    // thế state là gì ?
+    // ví dụ như máy tính thì state như là 1 memory nó lưu giữ các biến
+    // cập nhật data liên tục cho components mà không cần phải tải lại trang
 
-
+    handleChangeFirstName = (event) => {
+        this.setState({
+            firstName: event.target.value
+        })
+    }
+    handleChangeLastName = (event) => {
+        this.setState({
+            lastName: event.target.value
+        })
+    }
+    handleSubmit = (event) => {
+        event.preventDefault() // hàm này giúp không load lại trang khi submit
+        console.log('>>> data:', this.state)
+    }
     render() {
+        // console.log(this.state);
         return (
-            <div>
-                <div>hello components, My name is {this.state.name}</div>
-                <div>hello components, My name is {this.state.channel}</div>
-            </div>
+            <>
+                <form>
+                    <label htmlFor="fname" >First name:</label> <br />
+                    <input type="text" value={this.state.firstName} onChange={(event) => this.handleChangeFirstName(event)}></input><br />
+                    <label htmlFor="lname" >Last name:</label><br />
+                    <input type="text" value={this.state.lastName} onChange={(event) => this.handleChangeLastName(event)} ></input> <br />
+                    <input type="submit" value="Submit" onClick={(event) => this.handleSubmit(event)}></input>
+                </form>
+            </>
+
         )
     }
 }
